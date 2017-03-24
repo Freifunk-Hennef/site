ceb996e0e9f7a6a9e5db1fec8977fc59147c88a7
Automated Trigger <alariel@alariel.de>
2017-03-24 20:43:59 +0100

    Prepare automated build

--- site.mk
+++ site.mk
@@ -1,22 +1,19 @@
 GLUON_SITE_PACKAGES := \
-    gluon-mesh-batman-adv-15 \
     gluon-respondd \
     gluon-autoupdater \
-    gluon-config-mode-core \
     gluon-config-mode-autoupdater \
+    gluon-config-mode-contact-info \
+    gluon-config-mode-core \
+    gluon-config-mode-geo-location \
     gluon-config-mode-hostname \
     gluon-config-mode-mesh-vpn \
-    gluon-config-mode-geo-location \
-    gluon-config-mode-contact-info \
     gluon-ebtables-filter-multicast \
     gluon-ebtables-filter-ra-dhcp \
-    gluon-neighbour-info \
-    gluon-luci-private-wifi \
-    gluon-luci-admin \
-    gluon-luci-autoupdater \
-    gluon-luci-portconfig \
-    gluon-luci-wifi-config \
-    gluon-next-node \
+    gluon-web-admin \
+    gluon-web-autoupdater \
+    gluon-web-network \
+    gluon-web-wifi-config \
+    gluon-mesh-batman-adv-15 \
     gluon-mesh-vpn-fastd \
     gluon-radvd \
     gluon-status-page \
@@ -25,7 +22,7 @@ GLUON_SITE_PACKAGES := \
     iptables \
     haveged
 
-DEFAULT_GLUON_RELEASE := 2016.2-exp$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 2016.2-lede-$(shell date '+%Y%m%d')
 # Allow overriding the release number from the command line
 
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
@@ -35,4 +32,4 @@ GLUON_PRIORITY ?= 1
 GLUON_REGION ?= eu
 
 # Languages to include
-GLUON_LANGS ?= de en
+GLUON_LANGS ?= en de
